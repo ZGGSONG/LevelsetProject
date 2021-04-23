b4b8ff7594a0bc259d28be52a73bb4fecf419623
ZGGSONG <zggsong@foxmail.com>
2021-04-23 20:09:54 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import sys
+
 from graduateproject import mwin
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('../graduateproject/logo.png'))
     MainWindow = QMainWindow()
 
+    # 窗口
     ui = mwin.Ui_MainWindow()
     ui.setupUi(MainWindow)
 
