dc5c47374c9103a41fa6104c6fe5183442dcafef
ZGGSONG <zggsong@foxmail.com>
2021-05-06 16:18:19 +0800

    update

--- src/main.py
+++ src/main.py
@@ -4,24 +4,27 @@ import sys
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtGui
 
 from levelsetproject.mwin import Ui_MainWindow
 
 
 class MWim(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(MWim, self).__init__(parent)
+    def __init__(self):
+        super(MWim, self).__init__()
         self.setupUi(self)
         self.bn_image = ''
 
     @pyqtSlot()
     def on_img_tbtn_clicked(self):
-        file = QFileDialog.getOpenFileName(self, '选择一张图', '', 'images(*.png; *.jpg; *.jpeg; *.bmp);;*')
-        if file[0]:
-            self.bn_image = file[0]
+        imgName, imgType = QFileDialog.getOpenFileName(self, '选择一张图', '', 'images(*.png; *.jpg; *.jpeg; *.bmp);;*')
+        if imgType[0]:
+            self.bn_image = imgType[0]
         else:
             self.bn_image = './img/default.bmp'
         self.img_text.setText(f'{os.path.basename(self.bn_image)}')
+        img = QtGui.QPixmap(imgName).scaled(self.label.width(), self.label.height())
+        self.img_lab.setPixmap(img)
         return self.bn_image
 
 
